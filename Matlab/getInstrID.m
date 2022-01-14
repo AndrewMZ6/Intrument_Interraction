37a052f79118e61287c1df50c39ce0ae73edd96d
AndrewMZ <40640833+AndrewMZ6@users.noreply.github.com>
2022-01-14 16:57:43 +0700

    added new instruments

--- Matlab/getInstrID.m
+++ Matlab/getInstrID.m
@@ -1,4 +1,4 @@
-function [id] = getInstrInfo(model, connection)
+function [id] = getInstrID(model, connection)
 
 % function [id] = getInstrInfo(model, connection)
 %
@@ -19,18 +19,47 @@ end
 
 % Если на вход был подан только первый аргумент
 if (nargin<2) 
-    if (strcmpi(model, 'R&S')) % У модели R&S есть только LAN соединение
+    if (strcmpi(model, 'r&s')) % У модели R&S есть только LAN соединение
         connection = 'LAN';
     else
         connection = 'USB'; 
     end
 end
 
-% Если модель - это cxg
-if (strcmpi(model, 'cxg'))
-    if (strcmpi(connection, 'USB'))
-        id = 'USB0::0x0957::0x1F01::MY59100546::0::INSTR';
-    else
-        id = '192.168.0.85';
-    end
+% В зависимости от аргумента модели срабатывает case
+switch model
+    case 'cxg'
+        if (strcmpi(connection, 'USB'))
+            id = 'USB0::0x0957::0x1F01::MY59100546::0::INSTR';
+        else
+            id = '192.168.0.85';
+        end
+    case 'exa'
+        if (strcmpi(connection, 'USB'))
+            id = 'USB0::0x2A8D::0x1B0B::MY60240336::0::INSTR';
+        else
+            id = '192.168.0.73';
+        end
+    case 'wg'
+        if (strcmpi(connection, 'USB'))
+            id = 'USB0::0x0957::0x2807::MY57401328::0::INSTR';
+        else
+            id = '192.168.0.100';
+        end
+    case 'dsox'
+        if (strcmpi(connection, 'USB'))
+            id = 'USB0::0x2A8D::0x1797::CN58056332::0::INSTR';
+        else
+            error('DSOX осциллограф не имеет LAN соединения');
+        end
+    case 'r&s'
+        if (strcmpi(connection, 'USB'))
+            error('r&s анализатор не имеет USB соединения');
+        else
+            id = '192.168.073';
+        end
+    otherwise
+        error('Для выбранной модели идентификатора нет')
 end
+
+return;
