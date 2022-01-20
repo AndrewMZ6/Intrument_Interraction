72799a3a259b4ff223af4ac45fdbfed2575ace8c
AndrewMZ <40640833+AndrewMZ6@users.noreply.github.com>
2022-01-20 17:20:24 +0700

    Update main.m

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,14 +1,30 @@
-clear all;
+clc;
 close all;
 
 % Генерируем сигнал
-[ref, L, spec_pilot] = generateSig(50e6);
+[ref, L, spec_pilot] = generateSig(20e6);
 % Отправляем в генератор cxg
-sendToCxg(getInstrID('cxg'), ref,50e6);
+sendToCxg(getInstrID('cxg'), ref,20e6);
 % Получаем данные с анализатора exa
-a = getFromExa(getInstrID('exa'), 50e6);
+a = getFromExa(getInstrID('exa'), 20e6);
 % Демодулируем
 demod(a, ref, L, spec_pilot);
+
+
+%% Моделирование канала передачи
+
+h = zeros(1, 1024);
+h(1) = 1;
+h(10) = 0.75;
+h(50) = 0.5;
+H = fft(h);
+
+figure;
+subplot(2, 1, 1)
+plot(abs(H))
+subplot(2, 1, 2)
+plot(h)
+
 
 %%  WG + OSCI
 
